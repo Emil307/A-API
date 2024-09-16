7ecb38aa4cb1c01ef172b00ecdf4401d43b1a429
Emil307 <emilnovikov392@gmail.com>
2024-09-16 15:56:42 +0600

    [Feat] posts CRUD

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class PostsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+    return this.prisma.post.create({ data: createPostDto });
   }
 
   findAll() {
-    return `This action returns all posts`;
+    return this.prisma.post.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} post`;
+    return this.prisma.post.findFirst({ where: { id: id } });
   }
 
   update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+    return this.prisma.post.update({ where: { id: id }, data: updatePostDto });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} post`;
+    return this.prisma.post.delete({ where: { id: id } });
   }
 }
