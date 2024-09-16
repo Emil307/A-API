8eb46ce272925048c66ff69ec7bd7340fb3d99f5
Emil307 <emilnovikov392@gmail.com>
2024-09-16 16:01:34 +0600

    [Chore] Post Entity

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,1 +1,12 @@
-export class Post {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class PostEntity {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  body: string;
+
+  @ApiProperty()
+  createdAt: Date;
+}
