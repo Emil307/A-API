67bcd7b11f66bcfecef62fde5665a8fedc28becb
Emil307 <emilnovikov392@gmail.com>
2024-09-16 16:10:46 +0600

    [Chore] validation

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,6 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(400)
   @ApiProperty()
   body: string;
 }
